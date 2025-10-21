a94b1610a0609b6bc0c1eae4870458c2512509f5
Japa <btb1570@gmail.com>
2025-10-21 16:22:31 -0500

    open port 8080

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 
 	"grok-bot/bot"
 )
@@ -29,4 +30,9 @@ func main() {
 
 	// Run the bot with the loaded configuration
 	bot.RunWithConfig(config)
+
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
